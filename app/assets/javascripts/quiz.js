a29466b3086c3a8169afe33420cedb4ed131e212
Rebeca Souza <rebi_britto@hotmail.com>
2018-11-27 21:46:08 +0100

    styling quiz page

---
+++ app/assets/javascripts/quiz.js
@@ -0,0 +1,10 @@
+const next = document.querySelector(".next");
+const quiz = querySelectorAll(".quiz")
+const question1 = document.querySelector(".question1");
+const question2 = document.querySelector(".question2");
+ index += 1
+
+next.addEventListener("click", function(){
+  quiz.style.display == "none";
+  quiz.index.style.display =="block";
+});
