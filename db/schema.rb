5f437599977cceafd6afda7799fffaa6bab7baf6
Rebeca Souza <rebi_britto@hotmail.com>
2018-11-27 11:39:14 +0100

    navbar-footer base done

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_161758) do
     t.string "username"
     t.integer "score"
     t.string "city"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.integer "streak"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
