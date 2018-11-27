098bb5f512341facd4421b2cf5af5f4fb3f3650a
Jadwiga Coumert <jadwiga.coumert@gmail.com>
2018-11-27 11:20:58 +0100

    added an active column to the challenges table

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :challenges, except: [:destroy] do
+    resources :tips, except: [:destroy]
+  end
+
+  resources :user_challenges, except: [:edit, :update, :destroy]
+  resources :transportations, except: [:destroy]
 end
