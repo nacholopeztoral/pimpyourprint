587f014304cc1e137640ee9ad9fee99252aa219d
Rebeca Souza <rebi_britto@hotmail.com>
2018-11-28 18:29:46 +0100

    fixed java issue

--- app/javascript/packs/daily-challenge.js
+++ app/javascript/packs/daily-challenge.js
@@ -3,7 +3,10 @@ const reveal = document.getElementById("reveal");
 const unactiveCard = document.getElementById("unactive-challenge");
 const activeCard = document.getElementById("active-challenge");
 
-reveal.addEventListener("click", (event) => {
+
+
+if (reveal) {reveal.addEventListener("click", (event) => {
   unactiveCard.style.display = "inline";
   activeCard.style.display = "none";
 });
+};
