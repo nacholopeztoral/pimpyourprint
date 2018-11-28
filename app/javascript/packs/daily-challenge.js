4b1202b9473eb7db2e3228e67a4b073bd8a26305
Jadwiga <40387014+JadwigaFR@users.noreply.github.com>
2018-11-28 15:25:50 +0100

    Update daily-challenge.js

--- app/javascript/packs/daily-challenge.js
+++ app/javascript/packs/daily-challenge.js
@@ -4,7 +4,6 @@ const unactiveCard = document.getElementById("unactive-challenge");
 const activeCard = document.getElementById("active-challenge");
 
 reveal.addEventListener("click", (event) => {
-  console.log("hello")
   unactiveCard.style.display = "inline";
   activeCard.style.display = "none";
 });
