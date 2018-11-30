0485b5b3cd2bcde1fbc6c53293369e4271dd0a29
Anja Istenič <39512486+anjaist@users.noreply.github.com>
2018-11-30 14:17:53 +0100

    Update quiz_results_controller.rb

--- app/controllers/quiz_results_controller.rb
+++ app/controllers/quiz_results_controller.rb
@@ -15,7 +15,7 @@ class QuizResultsController < ApplicationController
     points = { 'Transport' => { 'Cycle' => 0, 'Walking' => 0, 'Drive' => 5, 'Public Transportation' => 3, 'My private jet' => 10 },
              'Flight' => {'None' => 0, 'Less than 10 hours' => 3 , 'Between 10-40 hours' => 5, 'More than 40 hours' => 10},
              'Smoker' => {'Never' => 0,  'Sometimes' => 5, 'Regurlaly' => 10 },
-             'Diet' => {'Never' => 0, '1 to 2 meals per week' => 3 ,'More than 3 meals per week' => 7,'Ever meal' => 10},
+             'Diet' => {'Never' => 0, '1 to 2 meals per week' => 3 ,'More than 3 meals per week' => 7,'Every meal' => 10},
              'Recicle' => {'Never' => 10, 'Sometimes' => 5, 'Most of the time' => 3,'Always' => 0 }
              }
   if points['Transport'][params[:transport]]|| points['Flight'][params[:flight]]|| points['Smoker'][params[:smoker]]|| points['Diet'][params[:diet]]|| points['Recicle'][params[:recicle]]
