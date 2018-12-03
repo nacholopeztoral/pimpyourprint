bb69ae72530b8e657c3db1f7cad80c521761e69c
Anja Istenic <aistenic1@gmail.com>
2018-12-03 18:29:26 +0100

    implemented user's time zone into streak

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -11,19 +11,24 @@ class DashboardsController < ApplicationController
     challenge_time = current_challenge.created_at.to_time
     # result of division is given back in seconds
     time_elapsed = (Time.current - challenge_time) / 3600
-    new_day = true
+    @new_day = true if new_day?
 
-    if (midnight? && !current_challenge.completed?) || time_elapsed > 48
+    if (@new_day && !current_challenge.completed?) || time_elapsed > 48
       @user.streak = 0
-    elsif time_elapsed < 24 && current_challenge.completed? && new_day
+    elsif current_challenge.completed? && @new_day
       @user.streak += 1
-      midnight? ? new_day = true : new_day = false # prevents streak increasing every time page is reloaded
+      @new_day = false # prevents streak increasing every time page is reloaded
     end
     @user.save
     authorize @user
   end
 
-  def midnight?
-    Time.current.hour == 23 && Time.current.min == 59 && Time.current.sec == 59
+  def new_day?
+    @user = current_user
+    current_challenge = @user.user_challenges.last
+    challenge_time = current_challenge.created_at.to_time
+    if Date.current.in_time_zone(@user.time_zone).to_date == challenge_time.to_date + 1
+      return true
+    end
   end
 end
