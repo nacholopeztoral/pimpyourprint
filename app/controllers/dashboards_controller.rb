823342d93bab06de1481add8a748dd4342a52ee8
Anja Istenic <aistenic1@gmail.com>
2018-12-04 15:30:45 +0100

    built base for scoring calculation

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,8 +2,11 @@ class DashboardsController < ApplicationController
   def show
     @user = current_user
     calculate_streak
+    calculate_score
     authorize @user
   end
+
+  private
 
   def calculate_streak
     @user = current_user
@@ -18,13 +21,28 @@ class DashboardsController < ApplicationController
         @user.streak_created_at = Time.current.in_time_zone(@user.time_zone)
       elsif current_challenge.completed? && !new_day?
         @user.streak = Time.current.in_time_zone(@user.time_zone).to_date + 1 - @user.streak_created_at.to_date
+        @user.highest_streak = @user.streak if @user.streak > @user.highest_streak
       elsif current_challenge.completed? && new_day?
         @user.streak = Time.current.in_time_zone(@user.time_zone).to_date - @user.streak_created_at.to_date
+        @user.highest_streak = @user.streak if @user.streak > @user.highest_streak
       end
     else
       @user.streak = 0
       @user.streak_created_at = Time.current.in_time_zone(@user.time_zone)
     end
+    @user.save
+    authorize @user
+  end
+
+  def calculate_score
+    @user = current_user
+    carbon_from_transport = 0.00
+    @user.transportations.each do |t|
+      carbon_from_transport += t.carbon / 1000.00
+    end
+    @user.score = 0 if @user.score.nil?
+    @user.score = @user.user_challenges.count(&:completed) * 15 +
+                  2 * @user.highest_streak - carbon_from_transport / 50
     @user.save
     authorize @user
   end
