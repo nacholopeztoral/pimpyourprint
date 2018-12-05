9c6596afba5b7acc611133f186f9b825d9427a5e
Jadwiga <40387014+JadwigaFR@users.noreply.github.com>
2018-12-05 16:22:17 +0100

    Update transportations_controller.rb

--- app/controllers/transportations_controller.rb
+++ app/controllers/transportations_controller.rb
@@ -9,7 +9,7 @@ class TransportationsController < ApplicationController
     @transportation = Transportation.new(carbon: carbon_g)
     @transportation.user_id = current_user.id
     authorize @transportation
-    if @transportation.save!
+    if @transportation.save
       redirect_to my_challenge_path
     else
       render 'new'
