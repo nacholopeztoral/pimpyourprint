6a6fa6b99f4a4a398deff1a83916f0af80c6fe4a
Anja Istenic <aistenic1@gmail.com>
2018-12-05 12:33:42 +0100

    added search functionality to Event model

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'pundit'
 gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave', '~> 1.2'
 gem "chartkick"
+gem 'pg_search'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   def index
     @events = policy_scope(Event)
+    @event = Event.search_by_city(params[:query])
     authorize @events
   end
 
